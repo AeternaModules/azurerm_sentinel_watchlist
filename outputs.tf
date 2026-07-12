e4f639641feac7b63c2f0a2f14434a4cc8c59419
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "sentinel_watchlists" {
-  description = "All sentinel_watchlist resources"
-  value       = azurerm_sentinel_watchlist.sentinel_watchlists
-}
 output "sentinel_watchlists_default_duration" {
-  description = "List of default_duration values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.default_duration]
+  description = "Map of default_duration values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.default_duration }
 }
 output "sentinel_watchlists_description" {
-  description = "List of description values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.description]
+  description = "Map of description values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.description }
 }
 output "sentinel_watchlists_display_name" {
-  description = "List of display_name values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.display_name]
+  description = "Map of display_name values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.display_name }
 }
 output "sentinel_watchlists_item_search_key" {
-  description = "List of item_search_key values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.item_search_key]
+  description = "Map of item_search_key values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.item_search_key }
 }
 output "sentinel_watchlists_labels" {
-  description = "List of labels values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.labels]
+  description = "Map of labels values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.labels }
 }
 output "sentinel_watchlists_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.log_analytics_workspace_id }
 }
 output "sentinel_watchlists_name" {
-  description = "List of name values across all sentinel_watchlists"
-  value       = [for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : v.name]
+  description = "Map of name values across all sentinel_watchlists, keyed the same as var.sentinel_watchlists"
+  value       = { for k, v in azurerm_sentinel_watchlist.sentinel_watchlists : k => v.name }
 }
 
